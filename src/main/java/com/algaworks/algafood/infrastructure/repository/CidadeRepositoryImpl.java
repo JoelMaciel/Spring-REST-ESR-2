d8f3f79f71ad913808665c3b2dfcccabc46b3148
JoelMaciel <soldado25461@gmail.com>
2021-02-22 01:35:51 -0300

    implemtents services Rest

--- src/main/java/com/algaworks/algafood/infrastructure/repository/CidadeRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CidadeRepositoryImpl.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.domain.model.Cidade;
@@ -37,9 +38,12 @@ public class CidadeRepositoryImpl  implements CidadeRepository{
 	
 	@Override
 	@Transactional
-	public void remover(Cidade cidade) {
+	public void remover(Long id) {
 		
-		cidade = buscar(cidade.getId());
+	Cidade cidade = buscar(id);
+		if (cidade == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
 		manager.remove(cidade);
 	}
 	
