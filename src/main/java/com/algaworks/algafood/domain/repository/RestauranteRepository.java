37bc3e5be31ee91d68be232fe36c26d1a3239e32
JoelMaciel <soldado25461@gmail.com>
2021-02-26 22:53:55 -0300

    JPQL @query

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -1,7 +1,13 @@
 package com.algaworks.algafood.domain.repository;
 
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
@@ -9,5 +15,18 @@ import com.algaworks.algafood.domain.model.Restaurante;
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 	
+	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFina);
+	
+	@Query("from Restaurante  where  nome like %:nome% and cozinha.id = :id")
+	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
+	
+	//List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
+
+	
+	Optional<Restaurante>  findFirstRestauranteByNomeContaining(String nome);
+	
+	List<Restaurante> findTop2ByNomeContaining(String nome);
+	
+	int countByCozinhaId (Long cozinha);
 
 }
