5b18408d071b2b20433ee9849e4dc7749c8a0127
JoelMaciel <soldado25461@gmail.com>
2021-02-27 01:59:10 -0300

    SDJ customizado error

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -39,11 +39,13 @@ public class TesteController {
 		return cozinhaRepository.findByNome(nome);
 	}
 	
-	@GetMapping("/restaurantes/por-taxa-frete")
+	/*@GetMapping("/restaurantes/por-taxa-frete")
 	public List<Restaurante>restaurantePorTaxaFrete(
 			BigDecimal taxaInicial, BigDecimal taxaFinal){
 		return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
-	}
+	}*/
+	
+	
 	
 	@GetMapping("/restaurantes/por-nome")
 	public List<Restaurante>restaurantePorTaxaFrete(
@@ -61,6 +63,12 @@ public class TesteController {
 	public List<Restaurante>restaurantesTop2PorNome(
 			String  nome){
 		return restauranteRepository.findTop2ByNomeContaining(nome);
+	}
+	
+	@GetMapping("/restaurantes/por-nome-e-frete")
+	public List<Restaurante> restaurantesPorNomeFrete(String nome,
+			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
+		return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
 	}
 	
 	@GetMapping("/restaurantes/count-por-cozinha")
