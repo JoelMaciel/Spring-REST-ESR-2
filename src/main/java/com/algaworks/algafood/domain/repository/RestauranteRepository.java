33ec4f64ad27ac9749b60702273d1cede58fb5e8
JoelMaciel <soldado25461@gmail.com>
2021-02-27 14:31:58 -0300

    erro, 404

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -1,36 +1,30 @@
 package com.algaworks.algafood.domain.repository;
 
-
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+public interface RestauranteRepository 
+		extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries {
+
+	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
-	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFina);
-	
-	//@Query("from Restaurante  where  nome like %:nome% and cozinha.id = :id")
+//	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
 	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
 	
-	//List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
-
+//	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
 	
-	Optional<Restaurante>  findFirstRestauranteByNomeContaining(String nome);
+	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
 	
 	List<Restaurante> findTop2ByNomeContaining(String nome);
 	
-	int countByCozinhaId (Long cozinha);
+	int countByCozinhaId(Long cozinha);
 	
-
-	List<Restaurante> find(String nome, BigDecimal taxaFreteInicial,
-			BigDecimal taxaFreteFinal);	
-
 }
--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -9,25 +9,26 @@ import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
 
 @Repository
-public class RestauranteRepositoryImpl {
-	
+public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
+
 	@PersistenceContext
 	private EntityManager manager;
 	
-	public List<Restaurante>  find(String nome, BigDecimal taxaFreteInicial,
-			BigDecimal taxaFreteFinal){
+	@Override
+	public List<Restaurante> find(String nome, 
+			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
 		
-		var jpql = " from  Restaurante  where nome like :nome" +
-			"and taxaFrete between :taxaInicial and  :taxaFinal";
+		var jpql = "from Restaurante where nome like :nome "
+				+ "and taxaFrete between :taxaInicial and :taxaFinal";
 		
 		return manager.createQuery(jpql, Restaurante.class)
-				.setParameter("nome", "%" +  nome + "%")
+				.setParameter("nome", "%" + nome + "%")
 				.setParameter("taxaInicial", taxaFreteInicial)
 				.setParameter("taxaFinal", taxaFreteFinal)
 				.getResultList();
-	} 
+	}
 	
-
 }
