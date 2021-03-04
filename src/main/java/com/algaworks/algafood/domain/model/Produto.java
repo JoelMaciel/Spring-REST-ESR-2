c9be8667f18643f2ff38075f4899f87f02b1341c
JoelMaciel <soldado25461@gmail.com>
2021-03-04 02:21:13 -0300

    Mapeando relacionamentos

--- src/main/java/com/algaworks/algafood/domain/model/Produto.java
+++ src/main/java/com/algaworks/algafood/domain/model/Produto.java
@@ -13,30 +13,30 @@ import javax.persistence.ManyToOne;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
- @Data
- @EqualsAndHashCode(onlyExplicitlyIncluded = true)
- @Entity
-	public class Produto {
-		@EqualsAndHashCode.Include
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Long id;
-		
-		@Column
-		private String nome;
-		
-		@Column
-		private String descricao;
-		
-		@Column
-		private BigDecimal preco;
-		
-		@Column
-		private Boolean ativo;
-		
-		
-		@ManyToOne
-		@JoinColumn(nullable = false)
-		private Restaurante restaurante;
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Produto {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String nome;
 	
+	@Column(nullable = false)
+	private String descricao;
+	
+	@Column(nullable = false)
+	private BigDecimal preco;
+	
+	@Column(nullable = false)
+	private Boolean ativo;
+
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Restaurante restaurante;
+
 }
