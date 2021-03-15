1dea87b4c84773dc977fbba9032842c06755423d
JoelMaciel <soldado25461@gmail.com>
2021-03-15 01:53:20 -0300

    Refatoraçao

--- src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
@@ -10,38 +10,54 @@ import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Estado;
 import com.algaworks.algafood.domain.repository.CidadeRepository;
-import com.algaworks.algafood.domain.repository.EstadoRepository;
 
 @Service
 public class CadastroCidadeService {
-	
+
+	private static final String MSG_CIDADE_EM_USO 
+		= "Cidade de código %d não pode ser removida, pois está em uso";
+
+	private static final String MSG_CIDADE_NAO_ENCONTRADA 
+		= "Não existe um cadastro de cidade com código %d";
+
 	@Autowired
 	private CidadeRepository cidadeRepository;
 	
-	 @Autowired
-     private EstadoRepository estadoRepository;
+	@Autowired
+	private CadastroEstadoService cadastroEstado;
 	
 	public Cidade salvar(Cidade cidade) {
 		Long estadoId = cidade.getEstado().getId();
+
+		Estado estado = cadastroEstado.buscarOuFalhar(estadoId);
 		
-		Estado estado = estadoRepository.findById(estadoId)
-				.orElseThrow(()-> new EntidadeNaoEncontradaException(
-				String.format("Nao existe cadastro  de cidade com o codigo %d", estadoId)));
+//		Estado estado = estadoRepository.findById(estadoId)
+//			.orElseThrow(() -> new EntidadeNaoEncontradaException(
+//					String.format("Não existe cadastro de estado com código %d", estadoId)));
 		
 		cidade.setEstado(estado);
+		
 		return cidadeRepository.save(cidade);
 	}
 	
 	public void excluir(Long cidadeId) {
 		try {
 			cidadeRepository.deleteById(cidadeId);
-		}catch (EmptyResultDataAccessException e) {
-			throw new  EntidadeNaoEncontradaException(
-					String.format("Nao existe um cadastro de cidade com o codigo %d", cidadeId));
-		}catch (DataIntegrityViolationException e) {
-			throw new  EntidadeEmUsoException(
-					String.format("Cidade de codigo %d nao pode ser removida", cidadeId));
+			
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontradaException(
+				String.format(MSG_CIDADE_NAO_ENCONTRADA, cidadeId));
+		
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(
+				String.format(MSG_CIDADE_EM_USO, cidadeId));
 		}
 	}
-
+	
+	public Cidade buscarOuFalhar(Long cidadeId) {
+		return cidadeRepository.findById(cidadeId)
+			.orElseThrow(() -> new EntidadeNaoEncontradaException(
+					String.format(MSG_CIDADE_NAO_ENCONTRADA, cidadeId)));
+	}
+	
 }
--- src/main/java/com/algaworks/algafood/domain/service/CadastroEstadoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroEstadoService.java
@@ -13,6 +13,11 @@ import com.algaworks.algafood.domain.repository.EstadoRepository;
 @Service
 public class CadastroEstadoService {
 	
+	private static final String MSG_ESTADO_NAO_ENCONTRADO = 
+			"Não existe um cadastro de estado com código %d";
+	private static final String MSG_ESTADO_EM_USO =
+			"Estado de código %d não pode ser removido, pois está em uso";
+	
 	@Autowired	
 	private EstadoRepository estadoRepository;
 	
@@ -23,13 +28,20 @@ public class CadastroEstadoService {
 	public void excluir (Long estadoId) {
 		try {
 			estadoRepository.deleteById(estadoId);
-		}catch (EmptyResultDataAccessException e) {
+			
+		} catch (EmptyResultDataAccessException e) {
 			throw new EntidadeNaoEncontradaException(
-					String.format("Nao existe  um cadastro de estado com este codigo %d", estadoId));
-		}catch (DataIntegrityViolationException e) {
+				String.format(MSG_ESTADO_NAO_ENCONTRADO, estadoId));
+
+		} catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(
-					String.format("Estado de codigo %d nao pode ser removido, pois ele esta em uso",estadoId));
+				String.format(MSG_ESTADO_EM_USO, estadoId));
 		}
+	}
+	public Estado buscarOuFalhar(Long estadoId) {
+	    return estadoRepository.findById(estadoId)
+	        .orElseThrow(() -> new EntidadeNaoEncontradaException(
+	                String.format(MSG_ESTADO_NAO_ENCONTRADO, estadoId)));
 	}
 
 }
