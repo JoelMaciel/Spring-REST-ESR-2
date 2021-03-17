492eb87f9c856f42468beadf7a651257145d8487
JoelMaciel <soldado25461@gmail.com>
2021-03-17 00:05:27 -0300

    respostas com a RFC 7807

---
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -0,0 +1,19 @@
+package com.algaworks.algafood.api.exceptionhandler;
+
+import lombok.Getter;
+
+@Getter
+public enum ProblemType {
+	
+	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade nao encontrada");
+	
+	private String title;
+	private String uri;
+	
+	private ProblemType(String path, String title) {
+
+		this.uri ="https://algafood.com.br" + path;
+		this.title = title;
+	}
+
+}
