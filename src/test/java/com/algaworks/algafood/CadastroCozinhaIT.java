aff7359f01dcf49413bcd06512f7d74444d3aa0e
JoelMaciel <soldado25461@gmail.com>
2021-03-25 02:19:04 -0300

    Testes de API com REST Assured

---
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -0,0 +1,36 @@
+package com.algaworks.algafood;
+
+import static io.restassured.RestAssured.given;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CadastroCozinhaIT {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Test
+	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		given()
+			.basePath("/cozinhas")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+		     .get()
+		.then()
+		     .statusCode(HttpStatus.OK.value());
+	}
+	
+
+}
+
