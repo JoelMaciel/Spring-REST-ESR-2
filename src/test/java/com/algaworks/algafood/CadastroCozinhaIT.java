c5bb81613a980637eba9ab826d4d5dce4b44680b
JoelMaciel <soldado25461@gmail.com>
2021-03-25 03:26:35 -0300

    Validando o corpo da resposta HTTP

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,6 +1,8 @@
 package com.algaworks.algafood;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,6 +31,21 @@ public class CadastroCozinhaIT {
 		     .get()
 		.then()
 		     .statusCode(HttpStatus.OK.value());
+	}
+	
+	@Test
+	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		given()
+			.basePath("/cozinhas")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+		     .get()
+		.then()
+		     .body("", hasSize(4))
+		     .body("nome",hasItems("Indiana", "Tailandesa"));
 	}
 	
 
